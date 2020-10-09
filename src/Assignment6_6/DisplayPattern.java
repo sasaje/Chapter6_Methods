bef45f72b4408c658eb988a7846961f801557b34
sarasandager <sara590x@edu.easj.dk>
2020-10-09 10:51:35 +0200

    Chapter 6 Methods - Assignment 6.6 changed

--- src/Assignment6_6/DisplayPattern.java
+++ src/Assignment6_6/DisplayPattern.java
@@ -1,12 +1,11 @@
 /*
  * Developed by Sara Sandager (sara590x@edu.easj.dk)
  * Licensed under the MIT License
- * 08/10/2020
+ * 09/10/2020
  *
  */
 
 package Assignment6_6;
-
 // Display patterns, write a method to display a pattern as follows:
 // 1
 // 12
@@ -14,30 +13,23 @@ package Assignment6_6;
 // ...
 // n n-1 ... 321
 
-import java.util.Scanner;
-
 public class DisplayPattern {
     public static void main(String[] args) {
-        displayPattern();
 
+        int maxValue = 8;
+
+        for (int i = 1; i <= maxValue; i++) {
+            visEnLinje(i);
+        }
     }
 
-    public static void displayPattern(){
-        System.out.println("Please, enter a number: ");
-        Scanner input = new Scanner(System.in);
-        int userInput = input.nextInt();
-
-        for (int x = 1; x <= userInput; x++) {
-            //The first row
-            for (int y = 1; y <= x; y++) {
-                System.out.print( y + " ");
-            }
-
-            //The same row as above but in reverse order
-            for (int y = x-1; y >= 1; y--) {
-                System.out.print( y + " ");
-            }
-            System.out.println(); // new line
+    public static void visEnLinje(int x){
+        for (int i = 1; i <= x; i++) {
+            System.out.print(i);
         }
+        for (int i = (x-1); i > 0; i--) {
+            System.out.print(i);
+        }
+        System.out.println("");
     }
 }
