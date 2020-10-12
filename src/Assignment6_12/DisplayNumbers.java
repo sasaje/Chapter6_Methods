4000d15d22c2fea9faa90914caec3ae0cd57f775
sarasandager <sara590x@edu.easj.dk>
2020-10-12 11:46:15 +0200

    Assignment 6_12 Display numbers solved

--- src/Assignment6_12/DisplayNumbers.java
+++ src/Assignment6_12/DisplayNumbers.java
@@ -13,30 +13,30 @@ public class DisplayNumbers {
     // Write a method that prints numbers using the following header:
     // public static void printNumbers(int num1, int num2, int numberPerLine)
     // This method prints characters between num1 and num2 with specified numbers per line. Write a test program that
-    // print ten characters per line from 1 to 100. Numbers are separated by exactly one space.
+    // print ten characters per line from 1 to 100.
 
 
     public static void main(String[] args) {
+        printNumbers(1,10,5);
+        printNumbers(10,100,10);
+    }
+
+    public static void printNumbers(int num1, int num2, int numberPerLine){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the first number: ");
-        int num1 = input.nextInt();
+        num1 = input.nextInt();
         System.out.println("Enter the second number: ");
-        int num2 = input.nextInt();
+        num2 = input.nextInt();
         System.out.println("Enter numbers per line: ");
-        int numberPerLine = input.nextInt();
+        numberPerLine = input.nextInt();
 
         System.out.println("Numbers between " + num1 + " and " + num2 + " is: ");
-        for (int i = num1; i <= num2; i++) {
-            for (int k = 1; k <= numberPerLine; k++) {
-                System.out.print(i + " ");
-            }
-            System.out.println("");
-        }
-    }
-    public static void printNumbers(int num1, int num2, int numberPerLine){
-
+        int counter = 1;
+        for (int i = num1; i <= num2; i++, counter++)
+            if (counter % numberPerLine == 0)
+                System.out.printf("%4d\n", i);
+            else
+                System.out.printf("%4d ", i);
 
     }
-
-
 }
